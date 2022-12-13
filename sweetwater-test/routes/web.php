07ffd289c2d674f09e05319ef8189060007ac4cd
ZacharyToney <zach@clearseries.com>
2022-12-13 17:54:45 -0600

    add views for task 1 and 2

--- sweetwater-test/routes/web.php
+++ sweetwater-test/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/task1', function () {
+    return view('task1');
+});
+
+Route::get('/task2', function () {
+    return view('task2');
+});
