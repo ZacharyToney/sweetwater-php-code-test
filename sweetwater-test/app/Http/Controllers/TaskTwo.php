c7c89c15804f926b8104b071ae8b19a99f7d0c5e
ZacharyToney <zach@clearseries.com>
2022-12-13 19:16:11 -0600

    second try on loop

--- sweetwater-test/app/Http/Controllers/TaskTwo.php
+++ sweetwater-test/app/Http/Controllers/TaskTwo.php
@@ -10,32 +10,32 @@ class TaskTwo extends Controller
         $sweetwater_test = DB::table('sweetwater_test')->get();
         foreach ($sweetwater_test as $record){
             //if date is in comments
-            //dd($record->comments);
-            if(str_contains(strtolower($record->comments),'expected ship date')){
-                if(date_parse($record->comments)){
-                    //get date from string
-                    $dateFromString = date_parse($record->comments);
-                    $year = $dateFromString['year'] ?: '';
-                    if(empty($year)){
-                        continue;
+            $loweredComments = strtolower($record->comments);
+            if(str_contains($loweredComments,'expected ship date')){
+                $commentSplit = explode('expected ship date',$loweredComments);
+                foreach ($commentSplit as $split){
+                    if(date_parse($split)){
+                        //get date from string
+                        $dateFromString = date_parse($record->comments);
+                        $year = $dateFromString['year'] ?: '';
+                        if(empty($year)){
+                            continue;
+                        }
+                        $month = $dateFromString['month'] ?: '';
+                        if(empty($month)){
+                            continue;
+                        }
+                        $day = $dateFromString['day'] ?: '';
+                        if(empty($day)){
+                            continue;
+                        }
+                        try {
+                            DB::update("update sweetwater_test set shipdate_expected = '$year-$month-$day' where orderid = ?",[$record->orderid]);
+                        }
+                        catch (\Exception $e){
+                            continue;
+                        }
                     }
-                    $month = $dateFromString['month'] ?: '';
-                    if(empty($month)){
-                        continue;
-                    }
-                    $day = $dateFromString['day'] ?: '';
-                    if(empty($day)){
-                        continue;
-                    }
-                    //sql wants YYYYMMDD
-                    $record->shipdate_expected = "$year$month$day";
-                    try {
-                        DB::update("update sweetwater_test set shipdate_expected = '$year-$month-$day' where orderid = ?",[$record->orderid]);
-                    }
-                    catch (\Exception $e){
-                        continue;
-                    }
-
                 }
             }
         }
