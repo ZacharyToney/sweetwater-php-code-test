8b4c0763e637bc262eaf471303c428197b95dd3e
ZacharyToney <zach@clearseries.com>
2022-12-13 18:02:28 -0600

    testing connection to db that it is working

--- sweetwater-test/app/Http/Controllers/TaskOne.php
+++ sweetwater-test/app/Http/Controllers/TaskOne.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TaskOne extends Controller
 {
     public function index()
     {
+        $sweetwater_test = DB::table('sweetwater_test')->get();
+        dd($sweetwater_test);
         return view('task1');
     }
 }
