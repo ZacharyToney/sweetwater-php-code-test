ad51b79a1efb43c611679e81069b7e6338f458af
ZacharyToney <zach@clearseries.com>
2022-12-13 18:16:53 -0600

    get all comments based off of text containing and rules established in task one and pass to view

--- sweetwater-test/app/Http/Controllers/TaskOne.php
+++ sweetwater-test/app/Http/Controllers/TaskOne.php
@@ -9,8 +9,45 @@ class TaskOne extends Controller
 {
     public function index()
     {
+
+
+        $commentsAboutCandy = [];
+        $commentsAboutCalls = [];
+        $commentsAboutReferredMe = [];
+        $commentsAboutSignatureRequirementsUponDelivery = [];
+        $commentsAboutMiscellaneousComments = [];
+
         $sweetwater_test = DB::table('sweetwater_test')->get();
-        dd($sweetwater_test);
-        return view('task1');
+        foreach ($sweetwater_test as $record){
+            //Comments about candy
+            if(str_contains(strtolower($record->comments),'candy')){
+                $commentsAboutCandy[] = $record;
+            }
+            //Comments about call me / don't call me
+            elseif(str_contains(strtolower($record->comments),'call me') || str_contains(strtolower($record->comments),"don't call me")){
+                $commentsAboutCalls[] = $record;
+            }
+            //Comments about who referred me
+            elseif(str_contains(strtolower($record->comments),'referred')){
+                $commentsAboutReferredMe[] = $record;
+            }
+            //Comments about signature requirements upon delivery
+            elseif(str_contains(strtolower($record->comments),'signature')){
+                $commentsAboutSignatureRequirementsUponDelivery[] = $record;
+            }
+            //Miscellaneous comments (everything else)
+            else{
+                $commentsAboutMiscellaneousComments[] = $record;
+            }
+        }
+        return view('task1',
+            [
+                'commentsAboutCandy'=>$commentsAboutCandy,
+                'commentsAboutCalls'=>$commentsAboutCalls,
+                'commentsAboutReferredMe'=>$commentsAboutReferredMe,
+                'commentsAboutSignatureRequirementsUponDelivery'=>$commentsAboutSignatureRequirementsUponDelivery,
+                'commentsAboutMiscellaneousComments'=>$commentsAboutMiscellaneousComments
+            ]
+        );
     }
 }
